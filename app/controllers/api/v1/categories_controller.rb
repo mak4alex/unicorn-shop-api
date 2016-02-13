12d009b9763432d26ba4611ee506af2babebafc9
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-13 12:55:40 +0300

    Add apidocs to category

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,14 +2,25 @@ class Api::V1::CategoriesController < ApplicationController
   respond_to :json
   before_action :set_category, only: [:show, :update, :destroy]
 
+  api! 'List all categories'
   def index
     respond_with Category.all
   end
 
+  api! 'Show category with id'
   def show
     render json: @category
   end
 
+  def_param_group :category do
+    param :category, Hash, required: true, action_aware: true do
+      param :title, String, required: true
+      param :description, String, required: true
+    end
+  end
+
+  api! 'Create category'
+  param_group :category
   def create
     @category = Category.new(category_params)
     if @category.save
@@ -19,6 +30,8 @@ class Api::V1::CategoriesController < ApplicationController
     end
   end
 
+  api! 'Update category with id'
+  param_group :category
   def update
     if @category.update(category_params)
       render json: @category, status: 200, location: [:api, @category]
@@ -27,10 +40,13 @@ class Api::V1::CategoriesController < ApplicationController
     end
   end
 
+  api! 'Destroy category with id'
   def destroy
     @category.destroy
     head 204
   end
+
+
 
   private
 
