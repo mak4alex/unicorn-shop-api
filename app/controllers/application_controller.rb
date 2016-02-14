19af193a4bfb025b027c53e91a3f6200b4768e8f
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-14 21:13:41 +0300

    Add greeting to root path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,13 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
+  def welcome
+    respond_to do |format|
+      format.html send_file "#{Rails.root}/public/index.html", type: 'text/html'
+      format.json { render json: { welcome: 'Hello from unicorn online-shop web-service',
+                                   date: Date.now }, status: 200 }
+    end
+  end
 
   def not_found
     api_error(status: 404, errors: ['Resource not found.'])
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  root to: 'application#welcome'
   apipie
-
   namespace :api, defaults: { format: :json } do
     scope module: :v1,
           constraints: ApiConstraints.new(version: 1, default: true) do
@@ -12,5 +12,4 @@ Rails.application.routes.draw do
       resources :products,   only: [:index, :show, :create, :update, :destroy]
     end
   end
-
 end
