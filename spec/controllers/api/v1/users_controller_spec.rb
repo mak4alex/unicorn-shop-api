761ab2b1b0891bbeba8a23c30850c962aa18d944
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-14 11:36:08 +0300

    Add index endpoint

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,18 +2,49 @@ require 'rails_helper'
 
 describe Api::V1::UsersController do
 
-  describe 'GET #show' do
+  describe 'GET #index' do
     before(:each) do
-      @user = create :user
-      get :show, id: @user.id
+      5.times { create :user }
+      get :index
     end
 
-    it 'returns the information about a reporter on a hash' do
-      user_response = json_response
-      expect(user_response[:email]).to eql @user.email
+    it 'returns the information about all users' do
+      category_response = json_response
+      expect(category_response[:users]).to have(5).items
     end
 
-    it { is_expected.to respond_with 200 }
+    it { should respond_with 200 }
+  end
+
+  describe 'GET #show' do
+
+    context 'when user exists' do
+      before(:each) do
+        @user = create :user
+        get :show, id: @user.id
+      end
+
+      it 'returns the information about a user on a hash' do
+        user_response = json_response[:user]
+        expect(user_response[:email]).to eql @user.email
+      end
+
+      it { is_expected.to respond_with 200 }
+    end
+
+    context 'when user does not exist' do
+      before(:each) do
+        get :show, id: 'not_exists'
+      end
+
+      it 'returns the errors' do
+        user_response = json_response
+        expect(user_response[:errors]).to include('Resource not found')
+      end
+
+      it { is_expected.to respond_with 404 }
+    end
+
   end
 
   describe 'POST #create' do
@@ -25,7 +56,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json representation for the user record just created' do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
 
@@ -65,7 +96,7 @@ describe Api::V1::UsersController do
       end
 
       it 'renders the json representation for the updated user' do
-        user_response = json_response
+        user_response = json_response[:user]
         expect(user_response[:email]).to eql 'newmail@example.com'
       end
 
