7a2224114ec605234e15c1976a4a2dbdc5f56a0c
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-16 21:23:44 +0300

    Setup google-fog

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :production do
   gem 'pg', '~> 0.18.4'
   gem 'rails_12factor', '~> 0.0.3'
   gem 'puma', '~> 2.16'
+  gem 'fog-google', '~> 0.1.1'
 end
 
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+        provider:                         'Google',
+        google_storage_access_key_id:     ENV['GOOGLE_ACCESS_KEY'],
+        google_storage_secret_access_key: ENV['GOOGLE_SECRET_KEY'],
+    }
+    config.fog_directory = ENV['GOOGLE_BUCKET']
+  end
+
 end
