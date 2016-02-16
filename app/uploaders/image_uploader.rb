df01cbaff022120e7faf0d40068e1d8487d8e0ff
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-16 21:28:48 +0300

    Fix bug

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
-    starage :fog
+    storage :fog
   else
     storage :file
   end
