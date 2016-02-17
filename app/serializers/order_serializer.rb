c70a3fb0da7d8ad8f90b788821e8a9dd7569028a
Alexander Maksimovich <mak4alex@gmail.com>
2016-02-17 21:39:20 +0300

    Adds a custom order product serializer to remove the user association

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,3 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :status, :total, :user_id, :pay_type, :created_at, :updated_at
+  has_many :products, serializer: OrderProductSerializer
 end
