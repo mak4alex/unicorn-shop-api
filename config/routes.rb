76cfd40bed66952f9262fc03444645ca3ea0ef84
Alexander Maksimovich <mak4alex@gmail.com>
2016-03-16 20:00:02 +0300

    Add admin routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   apipie
   namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User',  at: 'user'
+      mount_devise_token_auth_for 'Admin', at: 'admin'
+
       resources :users,      only: [:index, :show, :create, :update, :destroy]
       resources :categories, only: [:index, :show, :create, :update, :destroy] do
         get 'products',      on: :member
